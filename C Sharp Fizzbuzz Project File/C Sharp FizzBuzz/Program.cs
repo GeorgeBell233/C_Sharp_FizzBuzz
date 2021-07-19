f709a4d0127de6e5cc497756a1f1bbe117d8d138
GeorgeBell233 <georgeybell233@gmail.com>
2021-07-19 19:54:48 +0100

    Added C# Project

---
+++ C Sharp Fizzbuzz Project File/C Sharp FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class C_Sharp_FizzBuzz
+{
+	static string DivisibleByNumber(int Number, int Division, string Add, string Output)
+	{
+		if (Number % Division == 0)
+		{
+			return (Output += Add);
+		}
+		else
+		{
+			return Output;
+		}
+	}
+
+	public static void Main(String[] args)
+	{
+		Console.WriteLine("Enter the number you want FizzBuzz to go up to from 1");
+		int Number = Convert.ToInt32(Console.ReadLine());
+		for (int x = 1; x <= Number; ++x)
+		{
+			string output = "";
+
+			output = DivisibleByNumber(x, 3, "Fizz", output);
+			output = DivisibleByNumber(x, 5, "Buzz", output);
+
+			if (output == "")
+			{
+				output = Convert.ToString(x);
+			}
+
+			Console.Write(x);
+			Console.Write(": ");
+			Console.WriteLine(output);
+		}
+		Console.WriteLine("Press enter to close");
+		Console.ReadLine();
+	}
+}
